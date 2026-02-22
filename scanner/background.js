7b2f70f225d769889331f02163d06ce9890d5959
callumlewis05 <callumlewis569@gmail.com>
2026-02-22 04:08:29 +0000

    Added login functionality for retreiving auth from webapp.

--- scanner/background.js
+++ scanner/background.js
@@ -1,12 +1,26 @@
 let response_id = '';
 
+chrome.cookies.get(
+  {
+    url: 'https://example.com', // must match cookie domain
+    name: 'hackeurope_access_token'
+  },
+  (cookie) => {
+    if (cookie) {
+      console.log(cookie.value);
+    } else {
+      console.log('Cookie not found');
+    }
+  }
+);
+
 chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg.type === 'FLIGHT_DATA') {
     fetch("https://bd1e6ddc7c2b.ngrok.app/api/analyze", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6ImI5YmY0NTBjLTViYmUtNDNlOS05MjVmLTRmNGZhMmE4MmViYyIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2JqcXRjdG1sb25scXljYnZ5bXhzLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiI0NjI0YWMyZC01NGVhLTQxYTItYjYzZS0yM2ExNzZiMDQ1NDciLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzcxOTcxODc5LCJpYXQiOjE3NzE3MTI2NzksImVtYWlsIjoiY2FsbHVtQHRlc3QuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZnVsbF9uYW1lIjoiQ2FsbHVtIn0sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE3NzE3MTI2Nzl9XSwic2Vzc2lvbl9pZCI6IjExZmU3YWNmLWQ2MTEtNGJmNy04OWU3LTc4NzA1OGU5MjQ0ZSIsImlzX2Fub255bW91cyI6ZmFsc2V9.o0uhoqFyZPT4o1cWBvb9WndM-j_fjUsMJgVpr8GrAlwGXMLkPdonvyGepWhg0F4__ViGrIQ-Xn679cWCZGDqOg"
+        "Authorization": `Bearer ${cookie.value}`
       },
       body: JSON.stringify(msg.payload)
     })
@@ -38,7 +52,7 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6ImI5YmY0NTBjLTViYmUtNDNlOS05MjVmLTRmNGZhMmE4MmViYyIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2JqcXRjdG1sb25scXljYnZ5bXhzLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiI0NjI0YWMyZC01NGVhLTQxYTItYjYzZS0yM2ExNzZiMDQ1NDciLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzcxOTcxODc5LCJpYXQiOjE3NzE3MTI2NzksImVtYWlsIjoiY2FsbHVtQHRlc3QuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZnVsbF9uYW1lIjoiQ2FsbHVtIn0sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE3NzE3MTI2Nzl9XSwic2Vzc2lvbl9pZCI6IjExZmU3YWNmLWQ2MTEtNGJmNy04OWU3LTc4NzA1OGU5MjQ0ZSIsImlzX2Fub255bW91cyI6ZmFsc2V9.o0uhoqFyZPT4o1cWBvb9WndM-j_fjUsMJgVpr8GrAlwGXMLkPdonvyGepWhg0F4__ViGrIQ-Xn679cWCZGDqOg"
+        "Authorization": `Bearer ${cookie.value}`
       },
       body: JSON.stringify(msg.payload)
     })
